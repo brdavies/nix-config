6aa5df7b7c5c936841aa72ef518b0345b54e1076
Ben Davies <brdavies@gmail.com>
2026-01-31 12:28:07 +1000

    Remove default apps

--- modules/shared/home-manager.nix
+++ modules/shared/home-manager.nix
@@ -9,16 +9,16 @@ let name = "Ben Davies";
     enable = true;
     autocd = false;
     plugins = [
-      {
-        name = "powerlevel10k";
-        src = pkgs.zsh-powerlevel10k;
-        file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
-      }
-      {
-        name = "powerlevel10k-config";
-        src = lib.cleanSource ./config;
-        file = "p10k.zsh";
-      }
+      # {
+      #   name = "powerlevel10k";
+      #   src = pkgs.zsh-powerlevel10k;
+      #   file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+      # }
+      # {
+      #   name = "powerlevel10k-config";
+      #   src = lib.cleanSource ./config;
+      #   file = "p10k.zsh";
+      # }
     ];
 
     initContent = lib.mkBefore ''
